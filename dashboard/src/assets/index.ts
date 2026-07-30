2fb03586d6b2a8acfb4d52a0ea689815a767720b
Moneer .A Mizyed <xv.neer.business@gmail.com>
2026-07-30 15:57:29 +0300

    Update index.ts

--- dashboard/src/assets/index.ts
+++ dashboard/src/assets/index.ts
@@ -1,4 +1,4 @@
 import logo from "./logo.png"
 import emailGlow from "./email-glow.png"
-import horizontalLogo from "./horizontal-logo.png"
+import horizontalLogo from "./horizontal-logo.jpeg"
 export { logo, emailGlow, horizontalLogo }
